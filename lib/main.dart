196c0eb29049f9a5f2dd886003ffd8103fea513f
kototo522 <huyejiben@gmail.com>
2023-10-17 12:28:17 +0900

    add: english_wordsの追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,50 @@
-import 'package:flutter/foundation.dart';
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
       body: Column(
         children: [
+          Text('A random idea:'),
+          Text(appState.current.asLowerCase),
           ElevatedButton(
             onPressed: () {
-              if (kDebugMode) {
-                print('button pressed!');
-              }
+              print('button pressed!');
             },
-            child: const Text('Next'),
-          )
+            child: Text('Next'),
+          ),
         ],
       ),
     );
