adfb9946d1ca40c09d41f95e5e7405eb54a92a7a
kototo522 <huyejiben@gmail.com>
2023-10-17 14:19:57 +0900

    chenge: ファイルわけをしました

---
+++ lib/favoritePage.dart
@@ -0,0 +1,16 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './main.dart';
+
+class FavoritePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Text("favorite")],
+      ),
+    );
+  }
+}
